40171b662034d8ba98d276a80c231e35cb43ab1f
Muasya <francismuasya@gmail.com>
2021-02-19 13:07:59 +0300

    create Config Stuct and refactor parse config into Config::new

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,35 @@ use std::env;
 use std::fs;
 
 fn main() {
-    let args: Vec<String> = env::args().collect(); //collect cli arguments into a vector named args
+    //collect cli arguments into a vector named args
+    let args: Vec<String> = env::args().collect();
 
     // Saving the argument values in variables
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
     // 2. Read from the file entered
 
-    let contents = fs::read_to_string(filename).expect("something went wrong reading the file");
+    let contents =
+        fs::read_to_string(config.filename).expect("something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+// the struct Config will hold the two arguments
+// This glues together our data into a reusable obj rather than going the primitive obsession way
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        // create clones to borrow from args
+        // the clones will be less efficient on runtime
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config { query, filename }
+    }
 }
