cabb8f8af5ec91be106ff549466cb545807f37bd
Muasya <francismuasya@gmail.com>
2021-02-19 13:48:50 +0300

    refactor errors for the Config struct

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,21 @@
 
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     //collect cli arguments into a vector named args
     let args: Vec<String> = env::args().collect();
 
-    // Saving the argument values in variables
-    let config = Config::new(&args);
+    // Creating a new instance of Config
+    // in case we have less than two args the err from the new module in Config raises an error
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        // this anonymous function is called a closure
+        // unwrap_or_else creates a custom non-panic error handling
+        println!("Problem parsing arguments: {}", err);
+        // process::exit will stop the program immediately and return the number that was passed as the exit status code
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -31,11 +39,17 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
         // create clones to borrow from args
         // the clones will be less efficient on runtime
+        if args.len() < 3 {
+            // can return a panic here, but this is good for programming purposes
+            // returnng a result type is better than panic! in usage
+            // panic!("not enough arguments");
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
